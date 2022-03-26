4d29af7ac25b751b922e60a37db9515d53fc5fce
ctaggart2015 <99035696+ctaggart2015@users.noreply.github.com>
2022-03-26 13:07:14 -0400

    Add files via upload

---
+++ Resources.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-online/module_4/schools_complete.csv
+
+
+# In[2]:
+
+
+https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-online/module_4/students_complete.csv
+
+
+# In[ ]:
+
+
+
+
